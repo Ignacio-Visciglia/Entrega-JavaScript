e5a27a80d6b1616c470e8ca162a91ecc4bcb5de8
Juan Ignacio Visciglia <juanignaciovisciglia@hotmail.com>
2022-02-09 22:51:31 -0300

    Entrega Final

--- js/miArchivo.js
+++ js/miArchivo.js
@@ -1,31 +1,99 @@
-$(function () {
+/**
+ * @challenge: Entrega final curso JS CoderHouse
+ * 
+ * @version: v1.0.0
+ * @author: Juan Ignacio Visciglia
+ * @fecha: 09/02/2022
+ *
+ * History:
+ *  - v1.0.0 – Primera entrega
+ */
 
-  const urlGet = "http://localhost:3000/products";
- 
-  $.get(urlGet, function (response, status) {
-    let html =``;
-    if(status === "success"){
-      let allData = response;
-      for (const data of allData) {
-          html += `<!-- ${data.id} -->
-                  <li class="bg-dark">
-                    <figure>
-                      <img src=${data.img} alt="${data.name}">
-                    </figure>
-                    <figcaption>
-                      <p id="descripcion">${data.name}</p>
-                      <p id="precio">ARS$${data.price}</p>
-                      <button type="submit" id="btnAddItem${data.id}" class="btn btn-primary"><p>ADD TO CART</p><i class="fas fa-cart-plus"></i></button>
-                    </figcaption>
-                  </li>`
-      }
-      $(`.merch`).html(html);
+$(function(){
+
+//Product class
+    class Product{
+        constructor(obj){
+            this.id = obj.id;
+            this.img = obj.img;
+            this.name = obj.name;
+            this.price = obj.price;
+        }
     }
-  });
+
+    const URL = `http://localhost:3000/products`;
+
+//Get Method
+    $.get(URL, function(response, status){
+        if(status === `success`){
+            localStorage.setItem(`catalogue`, JSON.stringify(response));
+        }
+    })
 
 
-  const urlPost = 'http://localhost:3000/users';
+    const storage = JSON.parse(localStorage.getItem(`catalogue`));
+    let products = [];
 
+    for (const object of storage) {
+        products.push(new Product(object));
+    }
+    
+    const cart = [];
+    let total = 0;
+
+//E-commerce cards
+    for (const product of products) {
+        $(`.merch`).append(`<li class="bg-dark">
+                            <figure>
+                                <img src=${product.img} alt="${product.name}">
+                            </figure>
+                            <figcaption>
+                                <p id="descripcion">${product.name}</p>
+                                <p id="precio">ARS$${product.price}</p>
+                                <button type="button" id="btnAddItem${product.id}" class="btn btn-primary"><p>ADD TO CART</p><i class="fas fa-cart-plus"></i></button>
+                            </figcaption>
+                        </li>`);
+        //Add card to Cart                
+        $(`#btnAddItem${product.id}`).on(`click`, function(){
+            cart.push(product);
+            console.log(cart);
+            total += product.price;
+            
+            $('#cart').append(`<div class="row shoppingCartPlant mt-3 text-center">
+                                    <div class="col-3" id="productImg">
+                                        <img src=${product.img} class="imgModal"/>
+                                    </div>
+                                    <div class="col-3">
+                                        <h6>${product.name}</h6>
+                                    </div>
+                                    <div class="col-3">
+                                        <p class="product-price shoppingCartPlantPrice">${product.price}</p>
+                                    </div>
+                                    <div class="col-3">
+                                        <button class="btn btn-danger" id="remove-product-btn" data-bs-name="${product.name}">
+                                            x
+                                        </button>
+                                    </div>
+                                </div>`);
+            $(`.precioTotal`).html(total);
+        });
+    }
+
+//Delete Cart
+    $(`#deleteCartItems`).on(`click`, function(){ 
+        $('#cart').html(``);
+        $(`.precioTotal`).html(`0`);
+    })
+
+//Buy Cart
+    $(`#buyCart`).on(`click`, function(){ 
+        alert(`Thanks for buying Droid9 South American Products`);
+    })
+
+
+//Submit Sign Up    
+
+    const urlPost = 'http://localhost:3000/users';
 
     $('#submit').click(function(e){
         e.preventDefault();
@@ -38,13 +106,22 @@ $(function () {
                     password: password,
                     email: email,
                   }
-      
-
-        $.post(urlPost,body,function(response,status){
-            if(status==='success'){
-                $('#result').html('Signed Up')
-            }
-        })
+        if(userName === `` || password === `` || email === ``){
+            $('#result').html(`Fill the complete form and try again please`);
+            $('#result').hide(4000,function(){
+                $('#result').html(``);
+                $('#result').show(`fast`);
+            });
+        }else {
+            //Post Method
+            $.post(urlPost,body,function(response,status){
+                if(status==='success'){
+                    $('#result').html('Signed Up');
+                    $('#result').hide(10000000000);
+                }
+            })
+        }     
+        
     })
 
 });
--- jsDeprecado/elements.js
+++
@@ -1,6 +0,0 @@
-let btn = document.getElementById("btnAddItem");
-
-let descripcion = document.getElementById("descripcion");
-let precio =  document.getElementById("precio");
-
-let tableCarrito = document.getElementById("tableCarrito");
